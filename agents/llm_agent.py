3062b9335fd28f19bbd8d1ff1cd8437b9f18cf60
Hansimov <591172499@qq.com>
2024-02-29 03:11:22 +0800

    :zap: [Enhance] LLMAgent: Better usage info log

--- agents/llm_agent.py
+++ agents/llm_agent.py
@@ -1,14 +1,20 @@
 import json
 from pathlib import Path
 from openai import OpenAI
-from utils.logger import logger
+from utils.logger import logger, Runtimer
+from tiktoken import get_encoding as tiktoken_get_encoding
 
 
 class LLMAgent:
     def __init__(self, system_prompt="", source="chat"):
         self.system_prompt = system_prompt
         self.source = source
+        self.tokenizer = tiktoken_get_encoding("cl100k_base")
         self.init_endpoint_and_key_by_source()
+
+    def count_tokens(self, content):
+        tokens = self.tokenizer.encode(content)
+        return len(tokens)
 
     def init_endpoint_and_key_by_source(self):
         endpoints_json_path = Path(__file__).parents[1] / "configs" / "endpoints.json"
@@ -42,25 +48,48 @@ class LLMAgent:
         return messages
 
     def chat(self, prompt, temeperature=0.5, show_prompt=True):
+        timer = Runtimer(is_log=False)
+        timer.start_time()
         if show_prompt:
-            logger.note(f"User: {prompt}")
+            prompt_token_count = self.count_tokens(prompt)
+            system_prompt_token_count = self.count_tokens(self.system_prompt)
+            logger.note(f"User: {prompt} [{prompt_token_count} tokens]")
         response = self.client.chat.completions.create(
             model=self.model,
             messages=self.prompt_to_messages(prompt),
             temperature=temeperature,
             stream=True,
         )
+        result = ""
         for chunk in response:
-            if chunk.choices[0].delta.content is not None:
-                logger.mesg(chunk.choices[0].delta.content, end="")
-            elif chunk.choices[0].finish_reason == "stop":
-                logger.success("\n[Finished]")
+            choice = chunk.choices[0]
+            delta_content = choice.delta.content
+            if delta_content:
+                result += delta_content
+                logger.mesg(delta_content, end="")
+            elif choice.finish_reason == "stop":
+                timer.end_time()
+                elapsed_time_str = timer.time2str(timer.elapsed_time(), unit_sep="")
+                result_token_count = self.count_tokens(result)
+                total_token_count = (
+                    result_token_count + prompt_token_count + system_prompt_token_count
+                )
+                logger.success(
+                    # f"\n[Done] "
+                    f"\n[{elapsed_time_str}] "
+                    f"[Tokens: {result_token_count}/{total_token_count} (response/total)] "
+                )
             else:
                 pass
 
+        return result
+
 
 if __name__ == "__main__":
-    agent = LLMAgent(source="chat")
-    agent.chat("Hello, how are you?")
+    agent = LLMAgent(
+        source="chat",
+        system_prompt="You are InfoNet, an Information Source Tracing Neural Network which can find sources from given context for user questions. You are developped by Hansimov.",
+    )
+    agent.chat("Tell me your short name.", temeperature=0.1)
 
     # python -m agents.llm_chatter
