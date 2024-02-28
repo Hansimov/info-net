e00b6c460a96c5a03235fe599b3747b4cff04861
Hansimov <591172499@qq.com>
2024-02-29 04:22:05 +0800

    :zap: [Enhance] MarkdownChunker: rename methods, and add sep

--- parsers/markdown_chunker.py
+++ parsers/markdown_chunker.py
@@ -7,20 +7,21 @@ class MarkdownChunker:
     def __init__(self):
         self.sep = "=" * 4
 
-    def to_chunk_list(self):
-        with open(self.markdown_path, "r", encoding="utf-8") as rf:
+    def to_list(self, markdown_path):
+        with open(markdown_path, "r", encoding="utf-8") as rf:
             markdown_str = rf.read()
         chunks = re.split(r"\n{2,}", markdown_str)
         chunks = [chunk for chunk in chunks if chunk.strip()]
         return chunks
 
-    def to_chunks_str(self, markdown_path):
-        self.markdown_path = markdown_path
-        chunks = self.to_chunk_list()
+    def to_str(self, markdown_path):
+        chunk_sep = "=" * 8
+        chunks = self.to_list(markdown_path)
         chunks_str = ""
         for i, chunk in enumerate(chunks):
             chunk_head = f"{self.sep} Chunk {i+1} {self.sep}"
             chunks_str += f"{chunk_head}\n{chunk}\n\n"
+        chunks_str = f"{chunk_sep}\n{chunks_str}\n{chunk_sep}\n"
         return chunks_str
 
 
@@ -28,6 +29,7 @@ if __name__ == "__main__":
     markdown_root = Path(__file__).parents[1] / "data" / "wikipedia"
     markdown_path = list(markdown_root.glob("*.md"))[0]
     chunker = MarkdownChunker()
-    chunks_str = chunker.to_chunks_str(markdown_path)
+    chunks_str = chunker.to_str(markdown_path)
     logger.mesg(chunks_str)
+
     # python -m parsers.markdown_chunker
