8b404269849b199c6d4cf9384ec381917eebfcc0
Hansimov <591172499@qq.com>
2024-02-29 20:41:37 +0800

    :gem: [Feature] SourceIndexAgent: Better prompt, and scan multi chunks part

--- agents/source_index_agent.py
+++ agents/source_index_agent.py
@@ -1,3 +1,4 @@
+import time
 from agents.llm_agent import LLMAgent
 from parsers.markdown_chunker import MarkdownChunker
 from pathlib import Path
@@ -12,14 +13,13 @@ class SourceIndexAgent:
         self.chunker = MarkdownChunker()
         self.jsoner = JSONExtractor()
 
-    def construct_prompt_with_context(self, query, filepath):
-        chunks = self.chunker.to_list(filepath)
-        chunks_str = self.chunker.to_str(filepath)
-        chunk_info_prompt = f'{len(chunks)} chunks from file: "{filepath.name}"'
-        description_prompt = "You should treat symbols [1] as reference and citation."
+    def construct_prompt_with_context(self, query, chunks, offset=0, total_count=0):
+        chunks_str = self.chunker.chunks_to_str(chunks, offset=offset)
+        chunk_info_prompt = f"{len(chunks)} chunks from {offset+1} to {offset+len(chunks)} of {total_count} chunks."
+        description_prompt = ""
         task_prompt = "Your task is to find most related chunks to answer my query."
-        query_prompt = f"My query is: `{query}`"
-        output_format_prompt = "Your output should be a json list, which starts from '```json', and ends with '```'. The list items are the indexes of all the most related chunks to answer the query. Do not return unrelated chunks or too many chunks."
+        query_prompt = f"My query is: `{query}`."
+        output_format_prompt = 'The 1st output is your question analysis and chunks scanning. The 2nd output should be a json list ([]), starts from "```json", and ends with "```". The list items are indexe numbers of chunks most related to answer the query. Do not return unrelated or too many. Return [] if no chunk related.'
         prompt = (
             f"Here are {chunk_info_prompt}. {description_prompt} {task_prompt}\n"
             f"{query_prompt}\n"
@@ -28,35 +28,44 @@ class SourceIndexAgent:
             f"{output_format_prompt}\n"
             f"Remember, {query_prompt}\n"
         )
-        return prompt, chunks
+        return prompt
 
     def chat(self, prompt, filepath, show_prompt=True):
         if show_prompt:
             prompt_token_count = count_tokens(prompt)
             logger.note(f"User: {prompt} [{prompt_token_count} tokens]")
 
-        prompt_with_context, chunks = self.construct_prompt_with_context(
-            prompt, filepath
-        )
-        response_content = self.llm.chat(
-            prompt_with_context,
-            temperature=0.1,
-            max_tokens=int(2.5 * len(chunks)),
-            show_prompt=False,
-        )
+        chunks = self.chunker.md_to_chunks_list(markdown_path=filepath)
 
-        indexes = self.jsoner.to_obj(response_content)
-        indexes_and_chunks = [
-            {
-                "index": index,
-                "chunk": chunks[index - 1],
-            }
-            for index in indexes
-            if index <= len(chunks)
-        ]
+        indexes_and_chunks = []
+        chunk_num = 31
+        for offset in range(0, len(chunks), chunk_num):
+            chunks_part = chunks[offset : offset + chunk_num]
+
+            prompt_with_context = self.construct_prompt_with_context(
+                query=prompt, chunks=chunks_part, offset=offset, total_count=len(chunks)
+            )
+            response_content = self.llm.chat(
+                prompt_with_context,
+                temperature=0.1,
+                show_prompt=False,
+            )
+
+            indexes = self.jsoner.to_obj(response_content)
+            if indexes:
+                indexes_and_chunks_part = [
+                    {"index": index, "chunk": chunks[index - 1]}
+                    for index in indexes
+                    if offset <= index - 1 < offset + chunk_num
+                ]
+                indexes_and_chunks.extend(indexes_and_chunks_part)
+
+        indexes_and_chunks = sorted(indexes_and_chunks, key=lambda x: x["index"])
         for item in indexes_and_chunks:
             logger.note(f"=== Chunk {item['index']} ===")
             logger.line(f"{item['chunk']}")
+
+        logger.success(f"Related chunks {len(indexes_and_chunks)}/{len(chunks)}")
 
         return indexes_and_chunks
 
@@ -66,7 +75,10 @@ if __name__ == "__main__":
     markdown_root = Path(__file__).parents[1] / "data" / "wikipedia"
     markdown_path = list(markdown_root.glob("*.md"))[0]
     # query = "From what references can I know more about Three Laws of Robotics?"
-    query = "level 2 headers of this article"
+    query = "level 2 subheadings of toc in this doc"
+    # query = "summarize this article"
+    # query = "who is the partner of Olivaw?"
+    # query = "3th reference of this article"
     result = agent.chat(query, markdown_path)
 
     # python -m agents.source_index_agent
